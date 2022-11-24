5fee49c5ecec9a9a520a2e63611521edb8d7b8e9
excited <rifat1520154@gmail.com>
2022-11-24 15:33:00 +0600

    test_shopCart added

--- tests/test_shopCart.py
+++ tests/test_shopCart.py
@@ -1,16 +1,19 @@
 import pytest
-from pageObject.search import Search
 from pageObject.login import Login
+from pageObject.search import Search
+from pageObject.shoppingCart import ShoppingCart
 from pageObject.openBrowser import OpenBrowser
 
 
-class Test_003_search:
-    @pytest.mark.order(3)
-    def test_search(self, driver, config):
+class Test_004_shopCart:
+
+    @pytest.mark.order(4)
+    def test_shop(self, driver, config):
         open_url = OpenBrowser(driver)
         login = Login(driver)
         search = Search(driver)
+        shop_cart = ShoppingCart(driver)
         open_url.open_webBrowser(config)
         login.log_in()
         search.search_box()
-
+        shop_cart.shopping_cart()
